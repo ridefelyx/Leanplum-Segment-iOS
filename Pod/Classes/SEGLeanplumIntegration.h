c4993bcd520a77326d70ba68ce5f50f4991a22c5
Nikola Zagorchev <nikola.zagorchev@gmail.com>
2020-12-23 13:32:15 +0200

    update version

--- Pod/Classes/SEGLeanplumIntegration.h
+++ Pod/Classes/SEGLeanplumIntegration.h
@@ -5,9 +5,9 @@
 //  Copyright (c) 2020 Leanplum. All rights reserved.
 //
 
-#import <Analytics/SEGAnalytics.h>
+#import <Segment/SEGAnalytics.h>
 #import <Foundation/Foundation.h>
-#import "Leanplum.h"
+#import <Leanplum/Leanplum.h>
 
 @interface SEGLeanplumIntegration : NSObject <SEGIntegration>
 
